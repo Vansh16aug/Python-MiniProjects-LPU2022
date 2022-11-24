a6f9cf72af1e399f3118d6b70ca44606c26c339d
vansh kumar <112694631+Vansh16aug@users.noreply.github.com>
2022-11-24 20:24:14 +0530

    Update Project-13.py
    becoz we have to print till 10

--- Projects/Project-13.py
+++ Projects/Project-13.py
@@ -5,7 +5,7 @@ class MultiplyTable():
     def table(n):
         for i in range(2,n+1): # For every number between 2 and n
             print("\nMultiplication table for %d\n"%i)
-            for j in range(n+1):
+            for j in range(1,11):
                 print("%d x %d = %d"%(i,j,i*j))
 
 val=int(input("Enter the value for the table :"))
